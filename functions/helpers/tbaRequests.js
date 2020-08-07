075fc477529ae3b8c280b17e7e6c8e636e81dac9
ccans <55026208+ccans@users.noreply.github.com>
2020-08-06 23:45:31 -0500

    ok

---
+++ functions/helpers/tbaRequests.js
@@ -0,0 +1,35 @@
+
+async function gdfe(endpoint) {
+    var response = new Object;
+    // var url = new URL(`https://www.thebluealliance.com/api/v3/${endpoint}?X-TBA-Auth-Key=lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5`);
+    await fetch(`https://www.thebluealliance.com/api/v3/${endpoint}?X-TBA-Auth-Key=lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((respJson) => {
+            response = respJson;
+        });
+    return response;
+}
+
+async function getTeamDetails(teamNumber) {
+    // var teamNickName, teamName, teamLocation, teamImageString;
+    var response;
+    await gdfe(`team/frc${teamNumber}`)
+        .then((resp) => {
+    if(resp["Errors"].length >= 1) return response = {};
+            response.name = (resp.name) ? resp.name : null;
+            response.nickname = (resp.nickname) ? resp.nickname : null ;
+            response.location = (resp.state_prov && resp.city) ? `${resp.city}, ${resp.state_prov}` : "Not Available"
+        });
+    if(response == {}) return response;
+    await gdfe(`team/frc${teamNumber}/awards`)
+
+return response;
+}
+
+
+module.exports = {
+    gdfe,
+
+}
