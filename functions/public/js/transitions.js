45d570724e5664504a4afcdc96f70267ad6960d5
ccans <55026208+ccans@users.noreply.github.com>
2020-08-19 15:08:18 -0500

    penis music

--- functions/public/js/transitions.js
+++ functions/public/js/transitions.js
@@ -44,8 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
         views: [{
             namespace: 'index',
             beforeEnter(data) {
-                index();
-            }
+                console.log("sup");
+                  index();
+                  
+        
+                }
         }],
         transitions: [{
             async leave(data) {
@@ -72,15 +75,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function index() {
     
-    console.log("ok");
-    $.getScript("http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js", function( data, textStatus, jqxhr ) {
-        particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#1f2833"},"shape":{"type":"circle","stroke":{"width":0,"color":"#1f2833"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#c5c6c7","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+    $.ajax({
+        url: "http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js",
+        cache: true,
+        dataType: 'script',
+        success: function () {
+            particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#1f2833"},"shape":{"type":"circle","stroke":{"width":0,"color":"#1f2833"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#c5c6c7","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
         
+        }
     });
-    // $.getScript("../js/scrollanimations.js", function( data, textStatus, jqxhr ) {
-    //     console.log( "Load was performed." );
-    // });
-
 
 var lastScrollTop = 0;
 var page = 1;
